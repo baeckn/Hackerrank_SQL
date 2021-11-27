f984b17554760957ca3f1c6537e2986a2a000153
Nick Baeck <90761041+baeckn@users.noreply.github.com>
2021-11-27 09:21:16 +0900

    Update weather11.sql

--- SQL/Basic_Select/weather11.sql
+++ SQL/Basic_Select/weather11.sql
@@ -1,3 +1,5 @@
-SELECT DISTINCT(city) FROM station 
-WHERE LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
-OR RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+SELECT DISTINCT(city) 
+FROM station 
+WHERE 1=1
+  AND LEFT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+      OR RIGHT(city, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
